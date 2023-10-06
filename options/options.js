7f5722cf7263cd177dbbc89beeeabc2f44669441
Crowdbotics <team@crowdbotics.com>
2023-10-06 01:35:47 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "auto-dealership-43787",
+  url: "https://auto-dealership-43787.botics.co",
+  api: "https://auto-dealership-43787.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
